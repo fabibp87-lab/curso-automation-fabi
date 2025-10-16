417834b4d6697a9d87b58484e8fc5cd8b5b6e6a1
Fabiana <fabibp87@gmail.com>
2025-10-15 21:18:47 -0300

    Ejemplo de encapsulamiento

---
+++ src/main/java/org/automation/clase4/P1Estudiante.java
@@ -0,0 +1,16 @@
+package org.automation.clase4;
+
+public class P1Estudiante {
+    private int id;
+    private String nombreCompleto;
+    private float promedio;
+
+    public P1Estudiante(){
+        this.id = 0;
+        this.nombreCompleto = "";
+        this.promedio =5;
+    }
+    public P1Estudiante(int id, String nombreCompleto, float promedio){
+        
+    }
+}
