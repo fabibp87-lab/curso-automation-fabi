3a7f75d6aa810d481173b7dcb6998e554d2be711
Fabiana <fabibp87@gmail.com>
2025-10-22 14:56:28 -0300

    Se agrega tercer ejercicio a medias de la clase 4

---
+++ src/main/java/org/automation/clase4/Ej3Curso.java
@@ -0,0 +1,27 @@
+package org.automation.clase4;
+
+public class Ej3Curso {
+    private String nombre;
+    private int duracion;
+
+    public Ej3Curso(String nombre, int duracion){
+        this.nombre = nombre;
+        this.duracion = duracion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+}
