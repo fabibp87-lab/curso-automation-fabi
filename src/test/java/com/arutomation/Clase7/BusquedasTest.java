0ba9d10255cdbe800b717f750937ba4f3472857d
Fabiana <fabibp87@gmail.com>
2025-11-24 15:33:53 -0300

    Se agrega actualizacion clase 7

--- src/test/java/com/arutomation/Clase7/BusquedasTest.java
+++ src/test/java/com/arutomation/Clase7/BusquedasTest.java
@@ -1,9 +1,12 @@
 package com.arutomation.Clase7;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class BusquedasTest {
    private WebDriver driver;
@@ -16,6 +19,19 @@ public class BusquedasTest {
 
        //Sirve para abrir en incognito
        options.addArguments("incognito");
+
+       //Esto se usa en servidor. No se va a abrir una ventana sino por consola.A la vista no se ve.
+       //options.addArguments("headless");
+
+       //Indica cuanto va a esperar selenium en que abra la pagina.
+       options.setPageLoadTimeout(Duration.ofSeconds(60));
+       
+
+       //Pasamos las opciones anteriores o sea las instanciamos.
+       this.driver = new ChromeDriver(options);
+
+       //todo lo anterior es una configuracion basica del driver
+
 
    }
 
